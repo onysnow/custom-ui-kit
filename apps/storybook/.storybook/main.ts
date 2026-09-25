6a444bf45645e58c09678d2060fa563b78389fa3
Ony-Snow <anjoshannon@gmail.com>
2026-09-25 11:06:22 -0400

    feat: establish executable component library baseline

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,3 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+const config: StorybookConfig={stories:['../../../packages/ui/src/**/*.stories.@(ts|tsx)'],addons:['@storybook/addon-docs','@storybook/addon-a11y'],framework:{name:'@storybook/react-vite',options:{}}};
+export default config;
