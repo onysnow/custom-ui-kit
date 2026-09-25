76d10596e724d8fa3535bc7a3fdd9e2ed3b896b3
Ony-Snow <anjoshannon@gmail.com>
2026-09-25 11:03:49 -0400

    build: add packages/ui/vite.config.ts

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,4 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+export default defineConfig({plugins:[react(),dts({rollupTypes:true,tsconfigPath:'./tsconfig.json'})],build:{lib:{entry:'src/index.ts',formats:['es'],fileName:'index'},rollupOptions:{external:['react','react-dom','react/jsx-runtime']},cssFileName:'library'}});
